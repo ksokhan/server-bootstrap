d63042075bce5aaa3c4c17761ed4a60aba8dc468
Konstantin Sokhan <git@ksokhan.com>
2013-09-01 17:45:27 -0400

    updates

--- base_linux.sh
+++ base_linux.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 
+echo "> Changing root passwd"
 passwd &&
 
 apt-get update &&
-apt-get upgrade &&
+apt-get upgrade -y &&
 
 apt-get install build-essential nfs-common git-core curl -y -q &&
 
-apt-get install fail2ban unattended-upgrades &&
+apt-get install fail2ban unattended-upgrades -y -q &&
 
-adduser kostia sudo &&
+adduser kostia &&
+addgroup admin &&
+adduser kostia admin
 
-echo "if user doesnt have proper root, run `visudo`"
+echo "> Added user kostia"
 
 # APT::Periodic::Update-Package-Lists "1";
 # APT::Periodic::Download-Upgradeable-Packages "1";
 # APT::Periodic::AutocleanInterval "7";
 # APT::Periodic::Unattended-Upgrade "1";
-read -p 'Change intervals for Update-Package-Lists, Download-Upgradeable-Packages, Unattended-Upgrade to 1 day. autoclean to 7 days' &&
+echo "APT::Periodic::Unattended-Upgrade "1";" | sudo tee -a /etc/apt/apt.conf.d/10periodic
+read -p '> Change intervals for Update-Package-Lists, Download-Upgradeable-Packages, Unattended-Upgrade to 1 day. autoclean to 7 days' &&
+
 vim /etc/apt/apt.conf.d/10periodic
 
-# Edit the unnattended-upgrades file
-run sed -i -e 's/^\([[:space:]]*"Ubuntu raring-updates"\)/\/\/\1/' /etc/apt/apt.conf.d/50unattended-upgrades
+# mention unnattended-upgrades file
+echo "> Automatically upgrading only security packages."
+echo "> You can change what gets updated at /etc/apt/apt.conf.d/50unattended-upgrades"
+
